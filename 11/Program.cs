bb7b700e80f9ccc28bd9b9d626ebe2dd081a730a
an2niy85 <an2niy85@mail.ru>
2023-06-17 09:44:02 +0400

    Two solutions from second seminar IPTOC#

---
+++ 11/Program.cs
@@ -0,0 +1,9 @@
+﻿// 11 Напишите программу, которая выводит
+// случайное трехзначнное число и удаляет
+// вторую цифру этого числа.
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int num1 = num / 100;
+int num2 = num % 10;
+int num3 = num1*10 + num2;
+System.Console.WriteLine($"Число из крайних цифр числа {num} -> {num3}");
